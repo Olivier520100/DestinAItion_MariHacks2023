f19377abacd5684225b0e60bdc349965a027612d
Olivier520100 <olivier520100@gmail.com>
2023-04-01 12:30:09 -0400

    tests

--- components/flights.tsx
+++ components/flights.tsx
@@ -21,7 +21,6 @@ function Flights(props) {
     let flightP = []
     const [flights, setFlights] = useState([])
     const [selected, setSelected] = useState<Date[]>([]);
-    
     const handleSelect = (date: Date) => {
         const isSelected = selected.some((s) => dayjs(date).isSame(s, 'date'));
         if (isSelected) {
